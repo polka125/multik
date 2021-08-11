da46f7c839cb5d55de76eda4d29ec4e146bc53aa
polka125 <novozhilov1999@mail.ru>
2021-08-11 13:32:07 +0300

    first commit

--- multik-jvm/src/main/kotlin/org/jetbrains/kotlinx/multik/jvm/linalg/inverse.kt
+++ multik-jvm/src/main/kotlin/org/jetbrains/kotlinx/multik/jvm/linalg/inverse.kt
@@ -2,6 +2,7 @@ package org.jetbrains.kotlinx.multik.jvm.linalg
 
 import org.jetbrains.kotlinx.multik.api.identity
 import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.ndarray.complex.ComplexDouble
 import org.jetbrains.kotlinx.multik.ndarray.data.D2
 import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
 import org.jetbrains.kotlinx.multik.ndarray.data.MultiArray
@@ -14,4 +15,8 @@ internal fun invDouble(a: MultiArray<Double, D2>): D2Array<Double> {
 internal fun invFloat(a: MultiArray<Float, D2>): D2Array<Float> {
     requireSquare(a)
     return solveFloat(a, mk.identity(a.shape[0]))
+}
+
+internal fun invComplexDouble(a: MultiArray<ComplexDouble, D2>): D2Array<ComplexDouble> {
+    TODO("not yet implemented")
 }
